8c6aa30b9d01b6385da7b7e772282129986cba78
Meri Saarinen <mtmsaa@utu.fi>
2022-01-08 12:15:45 +0200

    Add functionality on filling the form

--- components/background.js
+++ components/background.js
@@ -1,37 +1,70 @@
 
 chrome.runtime.onMessage.addListener(async (request) => {
     console.log(request)
+    var label_text_dict = await readLabelJson();
+    console.log(label_text_dict)
+
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: setValueToInputElements,
+        args: [request, label_text_dict]
     });
 });
-function setValueToInputElements() {
-    var elements = document.getElementsByTagName("label"),
+function setValueToInputElements(request, label_text_dict) {
+    var labels = document.getElementsByTagName("label");
 
-        chosenEl = {}
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].innerText.toLowerCase().includes("työnantaja")) {
-            chosenEl = getNextInputSibling(elements[i]); break;
+    var keys = Object.keys(request);
+    for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        var value = request[key]
+        console.log("Value is ", value, "for key", key)
+        if (value == "") { continue; }
+
+        var chosenEl = null;
+        for (let idx = 0; idx < labels.length; idx++) {
+            if (labels[idx].innerText.toLowerCase().includes(getLabelName(key, label_text_dict))) {
+                chosenEl = getNextInputSibling(labels[idx]);
+                break;
+            }
+        }
+        if (chosenEl != null) {
+            changeElementValue(chosenEl, value)
         }
     }
-    changeElementValue(chosenEl, "test")
     function changeElementValue(element, value) {
+        console.log("element", element, "value", value)
         element.value = value;
         const event = new Event('change');
         element.dispatchEvent(event);
     }
     function getNextInputSibling(el) {
-        elementAcc = { ...el }
         for (let i = 0; i < 10; i++) {
-            el = el.nextElementSibling
-            if (el.tagName.toLowerCase() == "input") {
+            el = el?.nextElementSibling
+            if (el?.tagName.toLowerCase() == "input") {
+                console.log(el)
                 return el;
+            } else if (el?.getElementsByTagName("input").length > 0) {
+                console.log(el?.getElementsByTagName("input")[0])
+                return el?.getElementsByTagName("input")[0];
             }
         }
-        return null;
+        return {}
+    };
+
+    function getLabelName(id, label_text_dict) {
+        if (!label_text_dict.hasOwnProperty(id)) {
+            return "Not found.";
+        }
+        return label_text_dict[id];
     }
 }
-
+function readLabelJson() {
+    var url = chrome.runtime.getURL("/resources/label_text_fi.json").toString()
+    console.log(url)
+    var fetchResponse = "";
+    return fetch(url)
+        .then((response) => response.json())
+        .then(r => r["labelText"]);
+}
